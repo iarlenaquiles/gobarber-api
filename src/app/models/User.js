cbe7259e6389ea63581c35776e6e5ed285676c1b
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-23 01:14:05 -0300

    aplicando lint

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -14,8 +14,8 @@ class User extends Model {
       { sequelize }
     );
 
-    this.addHook('beforeSave', async (user) => {
-      if(user.password) {
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
     });
