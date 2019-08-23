fa04c694c50738b12d3e36f17238eaead5b23b6d
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-23 01:04:43 -0300

    rodando eslint

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,16 +2,19 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const userExists = await User.findOne({ where: { email: req.body.email}});
+    const userExists = await User.findOne({ where: { email: req.body.email } });
 
-    if(userExists){
-      return res.status(400).json({ error: 'User already exists'})
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
     }
 
-    const {id, name, email, provider } = await User.create(req.body);
+    const { id, name, email, provider } = await User.create(req.body);
 
     return res.json({
-      id, name, email, provider
+      id,
+      name,
+      email,
+      provider,
     });
   }
 }
