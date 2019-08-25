34722928714f8bfe65cf14db432e79949f4fe877
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-25 17:42:46 -0300

    validando a disponibilidade da data dos provider

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO } from 'date-fns';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -33,6 +33,26 @@ class AppointmentController {
     }
 
     const hourStart = startOfHour(parseISO(date));
+
+    // check for past dates
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permited' });
+    }
+
+    // check data availeble
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not availabe' });
+    }
 
     const appointment = await Appointment.create({
       user_id: req.userId,
