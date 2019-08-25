f37aae87916b93f48da876b7aa66a92388e0c226
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-25 18:16:33 -0300

    criando o controller da agenda do prestador e a rota

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,44 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import * as Yup from 'yup';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
+
+class ScheduleController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const checkUserProvider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true,
+      },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(400).json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,4 +24,5 @@ routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 routes.get('/providers', ProviderController.index);
 
+routes.get('/schedule', ScheduleController.index);
 export default routes;
