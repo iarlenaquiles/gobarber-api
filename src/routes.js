a03de46a00a8460a335b4fa628205dc073018112
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-26 00:53:32 -0300

    cancelando um agendamento

--- src/routes.js
+++ src/routes.js
@@ -23,6 +23,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
 routes.get('/providers', ProviderController.index);
 
 routes.get('/schedule', ScheduleController.index);
