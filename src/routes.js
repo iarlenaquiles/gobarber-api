106f449524fabe5854181c7525e3097613cb8cfe
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-26 02:34:03 -0300

    organizando as rotas

--- src/routes.js
+++ src/routes.js
@@ -16,17 +16,22 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
+
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 routes.delete('/appointments/:id', AppointmentController.delete);
+
 routes.get('/providers', ProviderController.index);
 
 routes.get('/schedule', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
 
