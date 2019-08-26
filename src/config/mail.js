06934fb42c4ebe6c4a32e2c36a2700f78ef6ba6f
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-26 01:15:08 -0300

    criando a config do email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'b8766d4386e7a4',
+    pass: '64870fb6bbc7c3',
+  },
+  default: {
+    from: 'Equipe GoPvt <noreply@gopvt.com>',
+  },
+};
