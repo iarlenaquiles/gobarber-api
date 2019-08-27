321c403e7381d7ef52863decb8eeda59748ac144
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-27 02:03:22 -0300

    criando controller de disponibilidade do provider

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -29,6 +30,7 @@ routes.get('/appointments', AppointmentController.index);
 routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/Available', AvailableController.index);
 
 routes.get('/schedule', ScheduleController.index);
 
