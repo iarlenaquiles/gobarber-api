d9aba21be596417d77f60fd30feb5d767cb0ea39
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-27 03:00:16 -0300

    criando variaveis de ambiente

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'etetetetetetetete',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'b8766d4386e7a4',
-    pass: '64870fb6bbc7c3',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoPvt <noreply@gopvt.com>',
